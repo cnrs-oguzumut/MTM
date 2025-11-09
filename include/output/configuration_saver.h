105d46d068d8213aacbedaed50dfce87546e76a4
oguzumut-cnrs <umutsalman@gmail.com>
2025-11-09 14:30:49 +0100

    shouldremeshed flag added

--- include/output/configuration_saver.h
+++ include/output/configuration_saver.h
@@ -100,7 +100,7 @@ public:
     double post_energy, 
     double post_stress,
     double pre_area,
-    double post_area
+    double post_area,
     bool shouldRemesh);  // Added plasticity flag parameter
 
     /**
